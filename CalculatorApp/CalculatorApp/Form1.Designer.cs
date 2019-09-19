2ef34b270c456cf50e44b88cb06f45cb941f7d5c
Md Ahsanul Hoque <ahrony90@gmail.com>
2019-09-19 00:11:04 -0300

    Added Simple Calculator Apps

---
+++ CalculatorApp/CalculatorApp/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace CalculatorApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbCakApp = new System.Windows.Forms.Label();
+            this.lblFirtNum = new System.Windows.Forms.Label();
+            this.lblSecondNum = new System.Windows.Forms.Label();
+            this.txtNumOne = new System.Windows.Forms.TextBox();
+            this.txtNumTwo = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnAddition = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbCakApp
+            // 
+            this.lbCakApp.AutoSize = true;
+            this.lbCakApp.Location = new System.Drawing.Point(367, 117);
+            this.lbCakApp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbCakApp.Name = "lbCakApp";
+            this.lbCakApp.Size = new System.Drawing.Size(90, 20);
+            this.lbCakApp.TabIndex = 0;
+            this.lbCakApp.Text = "Calculator";
+            // 
+            // lblFirtNum
+            // 
+            this.lblFirtNum.AutoSize = true;
+            this.lblFirtNum.Location = new System.Drawing.Point(128, 212);
+            this.lblFirtNum.Name = "lblFirtNum";
+            this.lblFirtNum.Size = new System.Drawing.Size(96, 20);
+            this.lblFirtNum.TabIndex = 1;
+            this.lblFirtNum.Text = "First Value";
+            // 
+            // lblSecondNum
+            // 
+            this.lblSecondNum.AutoSize = true;
+            this.lblSecondNum.Location = new System.Drawing.Point(128, 276);
+            this.lblSecondNum.Name = "lblSecondNum";
+            this.lblSecondNum.Size = new System.Drawing.Size(121, 20);
+            this.lblSecondNum.TabIndex = 2;
+            this.lblSecondNum.Text = "Second Value";
+            // 
+            // txtNumOne
+            // 
+            this.txtNumOne.Location = new System.Drawing.Point(326, 212);
+            this.txtNumOne.Name = "txtNumOne";
+            this.txtNumOne.Size = new System.Drawing.Size(100, 26);
+            this.txtNumOne.TabIndex = 3;
+            // 
+            // txtNumTwo
+            // 
+            this.txtNumTwo.Location = new System.Drawing.Point(326, 270);
+            this.txtNumTwo.Name = "txtNumTwo";
+            this.txtNumTwo.Size = new System.Drawing.Size(100, 26);
+            this.txtNumTwo.TabIndex = 4;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(136, 393);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(61, 20);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "Result";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(257, 393);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 20);
+            this.lblAnswer.TabIndex = 6;
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(140, 334);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 30);
+            this.btnMultiply.TabIndex = 7;
+            this.btnMultiply.Text = "X";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnAddition
+            // 
+            this.btnAddition.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddition.Location = new System.Drawing.Point(326, 332);
+            this.btnAddition.Name = "btnAddition";
+            this.btnAddition.Size = new System.Drawing.Size(75, 30);
+            this.btnAddition.TabIndex = 8;
+            this.btnAddition.Text = "+";
+            this.btnAddition.UseVisualStyleBackColor = true;
+            this.btnAddition.Click += new System.EventHandler(this.btnAddition_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(474, 334);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 30);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 464);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnAddition);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtNumTwo);
+            this.Controls.Add(this.txtNumOne);
+            this.Controls.Add(this.lblSecondNum);
+            this.Controls.Add(this.lblFirtNum);
+            this.Controls.Add(this.lbCakApp);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Calculator Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbCakApp;
+        private System.Windows.Forms.Label lblFirtNum;
+        private System.Windows.Forms.Label lblSecondNum;
+        private System.Windows.Forms.TextBox txtNumOne;
+        private System.Windows.Forms.TextBox txtNumTwo;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnAddition;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ CalculatorApp/CalculatorApp/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            double dVal1, dVal2;
+            dVal1 = GetFirstValue();
+            dVal2 = GetSecondValue();
+
+            if (dVal1 > 0 && dVal2 > 0)
+            {
+                lblAnswer.ForeColor = Color.Red;
+                lblAnswer.Text = "Result :" + (dVal1 * dVal2).ToString();
+            }
+        }
+        public double GetFirstValue()
+        {
+            double dNum1;
+            while(double.TryParse(txtNumOne.Text, out dNum1) == false)
+            {
+                lblAnswer.ForeColor = Color.Red;
+                lblAnswer.Text = "Value must be numeric and > 0";
+                txtNumOne.Text = "0";
+                txtNumOne.Focus();
+            }
+            return dNum1;
+        }
+
+        public double GetSecondValue()
+        {
+            double dNum2;
+            while (double.TryParse(txtNumTwo.Text, out dNum2) == false)
+            {
+                lblAnswer.ForeColor = Color.Red;
+                lblAnswer.Text = "Value must be numeric and > 0";
+                txtNumTwo.Text = "0";
+                txtNumTwo.Focus();
+            }
+            return dNum2;
+        }
+
+        private void btnAddition_Click(object sender, EventArgs e)
+        {
+            double dVal1, dVal2;
+            dVal1 = GetFirstValue();
+            dVal2 = GetSecondValue();
+
+            if (dVal1 > 0 && dVal2 > 0)
+            {
+                lblAnswer.ForeColor = Color.Red;
+                lblAnswer.Text = "Result" + (dVal1 + dVal2).ToString();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtNumOne.Text = "";
+            txtNumTwo.Text = "";
+            lblAnswer.Text = "";
+            txtNumOne.Focus();
+            txtNumTwo.Focus();
+        }
+    }
+}
