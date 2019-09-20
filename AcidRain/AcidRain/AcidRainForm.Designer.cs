5e5c5ab16a1fc224e24776d29d91e2e7c30a0a69
Md Ahsanul Hoque <ahrony90@gmail.com>
2019-09-20 13:00:31 -0300

    Added AcidRain Application

---
+++ AcidRain/AcidRain/AcidRainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace AcidRain
+{
+    partial class AcidRainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AcidRainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pHInputTextBox = new System.Windows.Forms.TextBox();
+            this.Process = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.resultRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(119, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter pH level";
+            // 
+            // pHInputTextBox
+            // 
+            this.pHInputTextBox.Location = new System.Drawing.Point(268, 59);
+            this.pHInputTextBox.Name = "pHInputTextBox";
+            this.pHInputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.pHInputTextBox.TabIndex = 7;
+            // 
+            // Process
+            // 
+            this.Process.Location = new System.Drawing.Point(409, 56);
+            this.Process.Name = "Process";
+            this.Process.Size = new System.Drawing.Size(90, 25);
+            this.Process.TabIndex = 2;
+            this.Process.Text = "Execute";
+            this.Process.UseVisualStyleBackColor = true;
+            this.Process.Click += new System.EventHandler(this.Process_Click);
+            // 
+            // result
+            // 
+            this.result.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Image = ((System.Drawing.Image)(resources.GetObject("result.Image")));
+            this.result.Location = new System.Drawing.Point(0, 101);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(692, 294);
+            this.result.TabIndex = 4;
+            // 
+            // resultRichTextBox
+            // 
+            this.resultRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultRichTextBox.Location = new System.Drawing.Point(188, 434);
+            this.resultRichTextBox.Name = "resultRichTextBox";
+            this.resultRichTextBox.Size = new System.Drawing.Size(294, 115);
+            this.resultRichTextBox.TabIndex = 5;
+            this.resultRichTextBox.Text = "Result will be displayed here..";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(520, 56);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(90, 25);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // AcidRainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(692, 590);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.resultRichTextBox);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.Process);
+            this.Controls.Add(this.pHInputTextBox);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "AcidRainForm";
+            this.Text = "AcidRainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pHInputTextBox;
+        private System.Windows.Forms.Button Process;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.RichTextBox resultRichTextBox;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ AcidRain/AcidRain/AcidRainForm.cs
@@ -0,0 +1,142 @@
+﻿
+/*
+  Progam name:  Acid Rain Application
+  Version:   1.1.1
+  #Description:  Displaing pH level and appropriate message based on user input 
+  Dependencies:  N/A
+  Database file: N/A
+  Change History:   N/A
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AcidRain
+{
+    public partial class AcidRainForm : Form
+    {
+        public AcidRainForm()
+        {
+            InitializeComponent();
+        }
+
+        //Process the data based on user input
+        private void Process_Click(object sender, EventArgs e)
+        {
+            String input = pHInputTextBox.Text;
+
+            double pHLevel;
+
+            if(double.TryParse(input, out pHLevel))
+            {
+                ShowResult(pHLevel);
+            }
+            else
+            {
+                MessageBox.Show("Invalid user input");
+            }
+            
+        }
+
+       
+        /*
+          Progam name:  ShowResult
+          Version:   1.1.1
+          Author: Md Ahsanul Hoque
+          #Description:  Perform logical Calculation based on the level of pH
+          Inputs:   Parameters- pHLevel
+          Outputs: Creates the appropriate messages
+          Dependencies:  N/A
+          Return value: none
+          Change History:   20190917 By Md Ahsanul Hoque
+        */
+
+        private void ShowResult(double pHLevl)
+        {
+            int[] rgb = new int[3];
+            String outputText = "";
+           
+            //Process pH level when less than 7.2 and greater than 0
+            if(pHLevl>=0 && pHLevl < 7.2)
+            {
+                if(pHLevl == 7)
+                {
+                    rgb[0] = 142;
+                    rgb[1] = 207;
+                    rgb[2] = 1;
+                    outputText = "Nutral, entered value is " + pHLevl;
+                    ShowMessage(outputText, rgb);
+                    outputText = "";
+                } else
+                {
+                    rgb[0] = 238;
+                    rgb[1] = 1;
+                    rgb[2] = 0;
+                    outputText = "Acidic and need to adjust pH increasers(bases) to balance pH Level, entered value is " + pHLevl;
+                    ShowMessage(outputText, rgb);
+                    outputText = "";
+                }
+            }
+
+            //process pH level when it is greater than 7.8 and less than 14
+            else if (pHLevl > 7.8 && pHLevl <= 14)
+            {
+                rgb[0] = 71;
+                rgb[1] = 87;
+                rgb[2] = 183;
+                outputText = "Basic or Alkaline and need to adjust pH decreasers(acid) to balance pH Level, entered value is " + pHLevl;
+                ShowMessage(outputText, rgb);
+                outputText = "";
+            }
+            //Inform if pH level is balanced
+            else if(pHLevl>=7.2 && pHLevl <= 7.8)
+            {
+                MessageBox.Show("Balanced");
+            }
+            else
+            {
+                resultRichTextBox.Text = "pH input level scale must be between 0 - 14";
+                resultRichTextBox.ForeColor = Color.Black;
+                resultRichTextBox.Focus();
+                MessageBox.Show("pH input level scale must be between 0 - 14");
+            }
+            
+        }
+
+
+        /*
+          Progam name:  ShowResult
+          Version:   1.1.1
+          Author: Md Ahsanul Hoque
+          #Description:  Show Appropriate messages
+          Inputs:   Parameters- message, rgb
+          Outputs: display the appropriate messages
+          Dependencies:  N/A
+          Return value: none
+          Change History:   20190917 By Md Ahsanul Hoque
+        */
+        private void ShowMessage(String message, int[] rgb)
+        {
+            resultRichTextBox.Text = message;
+            resultRichTextBox.ForeColor = Color.FromArgb(rgb[0],rgb[1],rgb[2]);
+            pHInputTextBox.Focus();
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            pHInputTextBox.Text = "";
+            resultRichTextBox.Text = "Result will be displayed here..";
+            resultRichTextBox.ForeColor = Color.Black;
+            pHInputTextBox.Focus();
+        }
+
+    }
+}
