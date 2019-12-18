bc5142b835d215cfef0a11182bc70f1e53f24ab5
Md Ahsanul Hoque <ahsan90@users.noreply.github.com>
2019-12-18 03:39:31 -0400

    Added RandySporting Club Applicaiton

---
+++ RandySporting/RandySporting/UI.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace RandySporting
+{
+    partial class UI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.twentyPercentRadioButton = new System.Windows.Forms.RadioButton();
+            this.tenPercentRadioButton = new System.Windows.Forms.RadioButton();
+            this.noneRadioButton = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.noSelectCheckBox = new System.Windows.Forms.CheckBox();
+            this.fullPackCheckBox = new System.Windows.Forms.CheckBox();
+            this.ExerciseCheckBox = new System.Windows.Forms.CheckBox();
+            this.nuitritionCheckBox = new System.Windows.Forms.CheckBox();
+            this.teamChckBox = new System.Windows.Forms.CheckBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.twentyPercentRadioButton);
+            this.groupBox1.Controls.Add(this.tenPercentRadioButton);
+            this.groupBox1.Controls.Add(this.noneRadioButton);
+            this.groupBox1.Location = new System.Drawing.Point(95, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 120);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Discount";
+            // 
+            // twentyPercentRadioButton
+            // 
+            this.twentyPercentRadioButton.AutoSize = true;
+            this.twentyPercentRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.twentyPercentRadioButton.Location = new System.Drawing.Point(16, 77);
+            this.twentyPercentRadioButton.Name = "twentyPercentRadioButton";
+            this.twentyPercentRadioButton.Size = new System.Drawing.Size(50, 19);
+            this.twentyPercentRadioButton.TabIndex = 2;
+            this.twentyPercentRadioButton.TabStop = true;
+            this.twentyPercentRadioButton.Text = "20%";
+            this.twentyPercentRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // tenPercentRadioButton
+            // 
+            this.tenPercentRadioButton.AutoSize = true;
+            this.tenPercentRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenPercentRadioButton.Location = new System.Drawing.Point(16, 52);
+            this.tenPercentRadioButton.Name = "tenPercentRadioButton";
+            this.tenPercentRadioButton.Size = new System.Drawing.Size(50, 19);
+            this.tenPercentRadioButton.TabIndex = 1;
+            this.tenPercentRadioButton.TabStop = true;
+            this.tenPercentRadioButton.Text = "10%";
+            this.tenPercentRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // noneRadioButton
+            // 
+            this.noneRadioButton.AutoSize = true;
+            this.noneRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noneRadioButton.Location = new System.Drawing.Point(16, 20);
+            this.noneRadioButton.Name = "noneRadioButton";
+            this.noneRadioButton.Size = new System.Drawing.Size(55, 19);
+            this.noneRadioButton.TabIndex = 0;
+            this.noneRadioButton.TabStop = true;
+            this.noneRadioButton.Text = "None";
+            this.noneRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.noSelectCheckBox);
+            this.groupBox2.Controls.Add(this.fullPackCheckBox);
+            this.groupBox2.Controls.Add(this.ExerciseCheckBox);
+            this.groupBox2.Controls.Add(this.nuitritionCheckBox);
+            this.groupBox2.Controls.Add(this.teamChckBox);
+            this.groupBox2.Location = new System.Drawing.Point(440, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(197, 181);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Service";
+            // 
+            // noSelectCheckBox
+            // 
+            this.noSelectCheckBox.AutoSize = true;
+            this.noSelectCheckBox.Location = new System.Drawing.Point(23, 113);
+            this.noSelectCheckBox.Name = "noSelectCheckBox";
+            this.noSelectCheckBox.Size = new System.Drawing.Size(87, 17);
+            this.noSelectCheckBox.TabIndex = 4;
+            this.noSelectCheckBox.Text = "No Selection";
+            this.noSelectCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // fullPackCheckBox
+            // 
+            this.fullPackCheckBox.AutoSize = true;
+            this.fullPackCheckBox.Location = new System.Drawing.Point(23, 90);
+            this.fullPackCheckBox.Name = "fullPackCheckBox";
+            this.fullPackCheckBox.Size = new System.Drawing.Size(88, 17);
+            this.fullPackCheckBox.TabIndex = 3;
+            this.fullPackCheckBox.Text = "Full Package";
+            this.fullPackCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ExerciseCheckBox
+            // 
+            this.ExerciseCheckBox.AutoSize = true;
+            this.ExerciseCheckBox.Location = new System.Drawing.Point(23, 67);
+            this.ExerciseCheckBox.Name = "ExerciseCheckBox";
+            this.ExerciseCheckBox.Size = new System.Drawing.Size(66, 17);
+            this.ExerciseCheckBox.TabIndex = 2;
+            this.ExerciseCheckBox.Text = "Exercise";
+            this.ExerciseCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // nuitritionCheckBox
+            // 
+            this.nuitritionCheckBox.AutoSize = true;
+            this.nuitritionCheckBox.Location = new System.Drawing.Point(23, 44);
+            this.nuitritionCheckBox.Name = "nuitritionCheckBox";
+            this.nuitritionCheckBox.Size = new System.Drawing.Size(67, 17);
+            this.nuitritionCheckBox.TabIndex = 1;
+            this.nuitritionCheckBox.Text = "Nuitrition";
+            this.nuitritionCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // teamChckBox
+            // 
+            this.teamChckBox.AutoSize = true;
+            this.teamChckBox.Location = new System.Drawing.Point(23, 21);
+            this.teamChckBox.Name = "teamChckBox";
+            this.teamChckBox.Size = new System.Drawing.Size(93, 17);
+            this.teamChckBox.TabIndex = 0;
+            this.teamChckBox.Text = "Team Building";
+            this.teamChckBox.UseVisualStyleBackColor = true;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(83, 346);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(241, 346);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(392, 346);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 4;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(562, 346);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(253, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 30);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Randy Sporting Club";
+            // 
+            // UI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "UI";
+            this.Text = "UI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton twentyPercentRadioButton;
+        private System.Windows.Forms.RadioButton tenPercentRadioButton;
+        private System.Windows.Forms.RadioButton noneRadioButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox noSelectCheckBox;
+        private System.Windows.Forms.CheckBox fullPackCheckBox;
+        private System.Windows.Forms.CheckBox ExerciseCheckBox;
+        private System.Windows.Forms.CheckBox nuitritionCheckBox;
+        private System.Windows.Forms.CheckBox teamChckBox;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ RandySporting/RandySporting/UI.cs
@@ -0,0 +1,123 @@
+﻿/*
+ Project: Calcuate the service costs for a customer for the service he/she choose
+ Author: Md Ahsanul Hoque
+ Since: 20190925
+ 
+ */
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandySporting
+{
+    public partial class UI : Form
+    {
+
+        //Declare the initial variables
+        private const decimal TEAM_BUILDING = 125.00m;
+        private const decimal NUITRTION = 60.00m;
+        private const decimal EXERCISE = 35.00m;
+        private const decimal FULL_PACKAGE = 200.00m;
+
+        private const decimal DISCOUNT_TEN = .10m;
+        private const decimal DISCOUNT_TWENTY = .20m;
+
+        decimal totalDue = 0m;
+
+
+        public UI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            //variable that holds the amount calculated 
+            decimal tempAmount = 0;
+            if (noSelectCheckBox.Checked == false)
+            {
+                if (fullPackCheckBox.Checked)
+                {
+                    tempAmount += FULL_PACKAGE;
+                }
+                else
+                {
+                    if (teamChckBox.Checked)
+                    {
+                        tempAmount += TEAM_BUILDING;
+                    }
+                    if (nuitritionCheckBox.Checked)
+                    {
+                        tempAmount += NUITRTION;
+                    }
+                    if (ExerciseCheckBox.Checked)
+                    {
+                        tempAmount += EXERCISE;
+                    }
+
+                }
+                MessageBox.Show("Calculated...!");
+            }
+            else
+            {
+                MessageBox.Show("You must uncheck No Selection option and select the services you want");
+            }
+
+
+            //Calculate the amount based on discount provided
+
+            if (tenPercentRadioButton.Checked)
+            {
+                tempAmount = tempAmount - tempAmount * DISCOUNT_TEN;
+                totalDue = tempAmount;
+            }
+            else if (twentyPercentRadioButton.Checked)
+            {
+                tempAmount = tempAmount - tempAmount * DISCOUNT_TWENTY;
+                totalDue = tempAmount;
+            }
+            else
+            {
+                totalDue = tempAmount;
+            }
+
+           
+
+        }
+
+       
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            totalDue = 0m;
+            teamChckBox.Checked = false;
+            nuitritionCheckBox.Checked = false;
+            ExerciseCheckBox.Checked = false;
+            fullPackCheckBox.Checked = false;
+            noSelectCheckBox.Checked = true;
+            noneRadioButton.Checked = true;
+            tenPercentRadioButton.Checked = false;
+            twentyPercentRadioButton.Checked = false;
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Total cost for the client : " + totalDue.ToString("C"));
+        }
+    }
+}
